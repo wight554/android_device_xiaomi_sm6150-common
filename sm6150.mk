41b47f6745ea3f51fc2664458db89a20be498ecb
Anthony Stange <stange@google.com>
2020-02-14 18:18:21 -0500

    sm6150-common: Migrate to Contexthub HAL 1.1
    Bug: 135951924
    Test: PTS
    Change-Id: Ia130bb3d1bb5b98487fc3c8f1da1b79919e18935

--- sm6150.mk
+++ sm6150.mk
@@ -161,8 +161,7 @@ PRODUCT_PACKAGES += \
 
 # Context hub HAL
 PRODUCT_PACKAGES += \
-    android.hardware.contexthub@1.0-impl.generic \
-    android.hardware.contexthub@1.0-service
+    android.hardware.contexthub@1.1-service.generic
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
