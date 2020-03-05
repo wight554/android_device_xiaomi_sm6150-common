15325ebf29809f3abbe9d125707e49c32690845b
Jack Wu <wjack@google.com>
2020-03-05 14:23:34 +0800

    sm6150-common: health: HAL 2.1
    Bug: 145943771
    Test: lshal debug android.hardware.health@2.1::IHealth/default
    Change-Id: I84f20459f5579e318d8e9f5f9ce68a733df27c53
    Signed-off-by: Jack Wu <wjack@google.com>

--- sm6150.mk
+++ sm6150.mk
@@ -231,7 +231,7 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-service
 
 # IFAA manager
 PRODUCT_PACKAGES += \
